54fc9583ca0cd6d276442c6c72b7bcf4cd306052
gabequinto <78443183+gabequinto@users.noreply.github.com>
2022-03-07 13:52:09 -0500

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install nginx -y
 sudo ufw allow 'Nginx HTTP'
-cd /var/www/html
+
 sudo rm index.nginx-debian.html
 sudo echo "Welcome to gabes SEC 350 webpage" > index.html
 sudo systemctl restart nginx
