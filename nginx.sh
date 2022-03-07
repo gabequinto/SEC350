abce0deb97685a8df8922bd8e8f5c216d9c3c2f8
gabequinto <78443183+gabequinto@users.noreply.github.com>
2022-03-07 13:01:28 -0500

    Create nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+index="/var/www/html/index.html
+sudo apt update
+sudo apt install nginx
+sudo ufw app list
+sudo ufw allow 'Nginx HTTP'
+sudo ufw status 
+systemctl status nginx
+cd /var/www/html
+sudo rm index.html
+sudo vi $index > /dev/null <<EOF
+  
+  Welcome to gabes webpage!
+EOF
+systemctl restart nginx
