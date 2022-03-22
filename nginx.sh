fa1a4bba52a797fba7d7b7b1ede6e705d912ef20
gabequinto <78443183+gabequinto@users.noreply.github.com>
2022-03-22 13:51:51 -0400

    Delete nginx.sh

--- nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install nginx -y
-sudo ufw allow 'Nginx HTTP'
-
-sudo rm /var/www/html/index.nginx-debian.html
-sudo echo "Welcome to gabes SEC 350 webpage" > index.html
-sudo systemctl restart nginx
